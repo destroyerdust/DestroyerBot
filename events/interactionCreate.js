2e7d73767d64cc278aeaf6e423ad6a3f1bf1424c
Sean Barkley <destroyerdust@gmail.com>
2025-11-02 13:52:14 -0500

    removed old log call

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,6 @@ const logger = require('../logger')
 module.exports = {
   name: 'interactionCreate',
   execute(interaction) {
-    // logger.info(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
     logger.info(`${interaction.user.tag} executed ${interaction.commandName}`)
   },
 }
